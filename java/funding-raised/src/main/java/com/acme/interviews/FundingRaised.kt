3b9310711c3667126d3ae14604291e593e097654
danielrotenberg <danirg10000@gmail.com>
2026-05-31 17:07:31 +0300

    * Converted FundingRaised.kt to Kotlin, all test pass

--- java/funding-raised/src/main/java/com/acme/interviews/FundingRaised.kt
+++ java/funding-raised/src/main/java/com/acme/interviews/FundingRaised.kt
@@ -1,197 +1,199 @@
-package com.acme.interviews;
+package com.acme.interviews
 
-import java.util.*;
+import com.opencsv.CSVReader
+import com.opencsv.exceptions.CsvValidationException
+import java.io.FileReader
+import java.io.IOException
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
+object FundingRaised {
+    @JvmStatic
+    @Throws(IOException::class)
+    fun where(options: MutableMap<String?, String?>): MutableList<MutableMap<String?, String?>?> {
+        var csvData: MutableList<Array<String?>?> = ArrayList<Array<String?>?>()
+        val reader = CSVReader(FileReader("startup_funding.csv"))
+        var row: Array<String?>? = null
 
-import java.io.FileReader;
-import java.io.IOException;
-
-public class FundingRaised {
-    public static List<Map<String, String>> where(Map<String, String> options) throws IOException {
-        List<String[]> csvData = new ArrayList<String[]>();
-        CSVReader reader = new CSVReader(new FileReader("startup_funding.csv"));
-        String[] row = null;
-
-        while(true) {
+        while (true) {
             try {
-                if (!((row = reader.readNext()) != null)) break;
-            } catch (CsvValidationException e) {
-                throw new RuntimeException(e);
+                if ((reader.readNext().also { row = it }) == null) break
+            } catch (e: CsvValidationException) {
+                throw RuntimeException(e)
             }
-            csvData.add(row);
+            csvData.add(row)
         }
 
-        reader.close();
-        csvData.remove(0);
+        reader.close()
+        csvData.removeAt(0)
 
-        if(options.containsKey("company_name")) {
-            List<String[]> results = new ArrayList<String[]> ();
+        if (options.containsKey("company_name")) {
+            val results: MutableList<Array<String?>?> = ArrayList<Array<String?>?>()
 
-            for(int i = 0; i < csvData.size(); i++) {
-                if(csvData.get(i)[1].equals(options.get("company_name"))) {
-                    results.add(csvData.get(i));
+            for (i in csvData.indices) {
+                if (csvData.get(i)!![1] == options.get("company_name")) {
+                    results.add(csvData.get(i))
                 }
             }
-            csvData = results;
+            csvData = results
         }
 
-        if(options.containsKey("city")) {
-            List<String[]> results = new ArrayList<String[]> ();
+        if (options.containsKey("city")) {
+            val results: MutableList<Array<String?>?> = ArrayList<Array<String?>?>()
 
-            for(int i = 0; i < csvData.size(); i++) {
-                if(csvData.get(i)[4].equals(options.get("city"))) {
-                    results.add(csvData.get(i));
+            for (i in csvData.indices) {
+                if (csvData.get(i)!![4] == options.get("city")) {
+                    results.add(csvData.get(i))
                 }
             }
-            csvData = results;
+            csvData = results
         }
 
-        if(options.containsKey("state")) {
-            List<String[]> results = new ArrayList<String[]> ();
+        if (options.containsKey("state")) {
+            val results: MutableList<Array<String?>?> = ArrayList<Array<String?>?>()
 
-            for(int i = 0; i < csvData.size(); i++) {
-                if(csvData.get(i)[5].equals(options.get("state"))) {
-                    results.add(csvData.get(i));
+            for (i in csvData.indices) {
+                if (csvData.get(i)!![5] == options.get("state")) {
+                    results.add(csvData.get(i))
                 }
             }
-            csvData = results;
+            csvData = results
         }
 
-        if(options.containsKey("round")) {
-            List<String[]> results = new ArrayList<String[]> ();
+        if (options.containsKey("round")) {
+            val results: MutableList<Array<String?>?> = ArrayList<Array<String?>?>()
 
-            for(int i = 0; i < csvData.size(); i++) {
-                if(csvData.get(i)[9].equals(options.get("round"))) {
-                    results.add(csvData.get(i));
+            for (i in csvData.indices) {
+                if (csvData.get(i)!![9] == options.get("round")) {
+                    results.add(csvData.get(i))
                 }
             }
-            csvData = results;
+            csvData = results
         }
 
-        List<Map<String, String>> output = new ArrayList<Map<String, String>>();
+        val output: MutableList<MutableMap<String?, String?>?> = ArrayList<MutableMap<String?, String?>?>()
 
-        for(int i = 0; i < csvData.size(); i++) {
-            Map<String, String> mapped = new HashMap<String, String> ();
-            mapped.put("permalink", csvData.get(i)[0]);
-            mapped.put("company_name", csvData.get(i)[1]);
-            mapped.put("number_employees", csvData.get(i)[2]);
-            mapped.put("category", csvData.get(i)[3]);
-            mapped.put("city", csvData.get(i)[4]);
-            mapped.put("state", csvData.get(i)[5]);
-            mapped.put("funded_date", csvData.get(i)[6]);
-            mapped.put("raised_amount", csvData.get(i)[7]);
-            mapped.put("raised_currency", csvData.get(i)[8]);
-            mapped.put("round", csvData.get(i)[9]);
-            output.add(mapped);
+        for (i in csvData.indices) {
+            val mapped: MutableMap<String?, String?> = HashMap<String?, String?>()
+            mapped.put("permalink", csvData.get(i)!![0])
+            mapped.put("company_name", csvData.get(i)!![1])
+            mapped.put("number_employees", csvData.get(i)!![2])
+            mapped.put("category", csvData.get(i)!![3])
+            mapped.put("city", csvData.get(i)!![4])
+            mapped.put("state", csvData.get(i)!![5])
+            mapped.put("funded_date", csvData.get(i)!![6])
+            mapped.put("raised_amount", csvData.get(i)!![7])
+            mapped.put("raised_currency", csvData.get(i)!![8])
+            mapped.put("round", csvData.get(i)!![9])
+            output.add(mapped)
         }
 
-        return output;
+        return output
     }
 
-    public static Map<String, String> findBy(Map<String, String> options) throws IOException, NoSuchEntryException {
-        List<String[]> csvData = new ArrayList<String[]>();
-        CSVReader reader = new CSVReader(new FileReader("startup_funding.csv"));
-        String[] row = null;
+    @JvmStatic
+    @Throws(IOException::class, NoSuchEntryException::class)
+    fun findBy(options: MutableMap<String?, String?>): MutableMap<String?, String?> {
+        val csvData: MutableList<Array<String?>?> = ArrayList<Array<String?>?>()
+        val reader = CSVReader(FileReader("startup_funding.csv"))
+        var row: Array<String?>? = null
 
-        while(true) {
+        while (true) {
             try {
-                if (!((row = reader.readNext()) != null)) break;
-            } catch (CsvValidationException e) {
-                throw new RuntimeException(e);
+                if ((reader.readNext().also { row = it }) == null) break
+            } catch (e: CsvValidationException) {
+                throw RuntimeException(e)
             }
-            csvData.add(row);
+            csvData.add(row)
         }
 
-        reader.close();
-        csvData.remove(0);
-        Map<String, String> mapped = new HashMap<String, String> ();
+        reader.close()
+        csvData.removeAt(0)
+        val mapped: MutableMap<String?, String?> = HashMap<String?, String?>()
 
-        for(int i = 0; i < csvData.size(); i++) {
-            if(options.containsKey("company_name")) {
-                if(csvData.get(i)[1].equals(options.get("company_name"))) {
-                    mapped.put("permalink", csvData.get(i)[0]);
-                    mapped.put("company_name", csvData.get(i)[1]);
-                    mapped.put("number_employees", csvData.get(i)[2]);
-                    mapped.put("category", csvData.get(i)[3]);
-                    mapped.put("city", csvData.get(i)[4]);
-                    mapped.put("state", csvData.get(i)[5]);
-                    mapped.put("funded_date", csvData.get(i)[6]);
-                    mapped.put("raised_amount", csvData.get(i)[7]);
-                    mapped.put("raised_currency", csvData.get(i)[8]);
-                    mapped.put("round", csvData.get(i)[9]);
+        for (i in csvData.indices) {
+            if (options.containsKey("company_name")) {
+                if (csvData.get(i)!![1] == options.get("company_name")) {
+                    mapped.put("permalink", csvData.get(i)!![0])
+                    mapped.put("company_name", csvData.get(i)!![1])
+                    mapped.put("number_employees", csvData.get(i)!![2])
+                    mapped.put("category", csvData.get(i)!![3])
+                    mapped.put("city", csvData.get(i)!![4])
+                    mapped.put("state", csvData.get(i)!![5])
+                    mapped.put("funded_date", csvData.get(i)!![6])
+                    mapped.put("raised_amount", csvData.get(i)!![7])
+                    mapped.put("raised_currency", csvData.get(i)!![8])
+                    mapped.put("round", csvData.get(i)!![9])
                 } else {
-                    continue;
+                    continue
                 }
             }
 
-            if(options.containsKey("city")) {
-                if(csvData.get(i)[4].equals(options.get("city"))) {
-                    mapped.put("permalink", csvData.get(i)[0]);
-                    mapped.put("company_name", csvData.get(i)[1]);
-                    mapped.put("number_employees", csvData.get(i)[2]);
-                    mapped.put("category", csvData.get(i)[3]);
-                    mapped.put("city", csvData.get(i)[4]);
-                    mapped.put("state", csvData.get(i)[5]);
-                    mapped.put("funded_date", csvData.get(i)[6]);
-                    mapped.put("raised_amount", csvData.get(i)[7]);
-                    mapped.put("raised_currency", csvData.get(i)[8]);
-                    mapped.put("round", csvData.get(i)[9]);
+            if (options.containsKey("city")) {
+                if (csvData.get(i)!![4] == options.get("city")) {
+                    mapped.put("permalink", csvData.get(i)!![0])
+                    mapped.put("company_name", csvData.get(i)!![1])
+                    mapped.put("number_employees", csvData.get(i)!![2])
+                    mapped.put("category", csvData.get(i)!![3])
+                    mapped.put("city", csvData.get(i)!![4])
+                    mapped.put("state", csvData.get(i)!![5])
+                    mapped.put("funded_date", csvData.get(i)!![6])
+                    mapped.put("raised_amount", csvData.get(i)!![7])
+                    mapped.put("raised_currency", csvData.get(i)!![8])
+                    mapped.put("round", csvData.get(i)!![9])
                 } else {
-                    continue;
+                    continue
                 }
             }
 
-            if(options.containsKey("state")) {
-                if(csvData.get(i)[5].equals(options.get("state"))) {
-                    mapped.put("permalink", csvData.get(i)[0]);
-                    mapped.put("company_name", csvData.get(i)[1]);
-                    mapped.put("number_employees", csvData.get(i)[2]);
-                    mapped.put("category", csvData.get(i)[3]);
-                    mapped.put("city", csvData.get(i)[4]);
-                    mapped.put("state", csvData.get(i)[5]);
-                    mapped.put("funded_date", csvData.get(i)[6]);
-                    mapped.put("raised_amount", csvData.get(i)[7]);
-                    mapped.put("raised_currency", csvData.get(i)[8]);
-                    mapped.put("round", csvData.get(i)[9]);
+            if (options.containsKey("state")) {
+                if (csvData.get(i)!![5] == options.get("state")) {
+                    mapped.put("permalink", csvData.get(i)!![0])
+                    mapped.put("company_name", csvData.get(i)!![1])
+                    mapped.put("number_employees", csvData.get(i)!![2])
+                    mapped.put("category", csvData.get(i)!![3])
+                    mapped.put("city", csvData.get(i)!![4])
+                    mapped.put("state", csvData.get(i)!![5])
+                    mapped.put("funded_date", csvData.get(i)!![6])
+                    mapped.put("raised_amount", csvData.get(i)!![7])
+                    mapped.put("raised_currency", csvData.get(i)!![8])
+                    mapped.put("round", csvData.get(i)!![9])
                 } else {
-                    continue;
+                    continue
                 }
             }
 
-            if(options.containsKey("round")) {
-                if(csvData.get(i)[9].equals(options.get("round"))) {
-                    mapped.put("permalink", csvData.get(i)[0]);
-                    mapped.put("company_name", csvData.get(i)[1]);
-                    mapped.put("number_employees", csvData.get(i)[2]);
-                    mapped.put("category", csvData.get(i)[3]);
-                    mapped.put("city", csvData.get(i)[4]);
-                    mapped.put("state", csvData.get(i)[5]);
-                    mapped.put("funded_date", csvData.get(i)[6]);
-                    mapped.put("raised_amount", csvData.get(i)[7]);
-                    mapped.put("raised_currency", csvData.get(i)[8]);
-                    mapped.put("round", csvData.get(i)[9]);
+            if (options.containsKey("round")) {
+                if (csvData.get(i)!![9] == options.get("round")) {
+                    mapped.put("permalink", csvData.get(i)!![0])
+                    mapped.put("company_name", csvData.get(i)!![1])
+                    mapped.put("number_employees", csvData.get(i)!![2])
+                    mapped.put("category", csvData.get(i)!![3])
+                    mapped.put("city", csvData.get(i)!![4])
+                    mapped.put("state", csvData.get(i)!![5])
+                    mapped.put("funded_date", csvData.get(i)!![6])
+                    mapped.put("raised_amount", csvData.get(i)!![7])
+                    mapped.put("raised_currency", csvData.get(i)!![8])
+                    mapped.put("round", csvData.get(i)!![9])
                 } else {
-                    continue;
+                    continue
                 }
             }
 
-            return mapped;
+            return mapped
         }
 
-        throw new NoSuchEntryException();
+        throw NoSuchEntryException()
     }
 
-    public static void main(String[] args) {
+    @JvmStatic
+    fun main(args: Array<String>) {
         try {
-            Map<String, String> options = new HashMap<String, String> ();
-            options.put("company_name", "Facebook");
-            options.put("round", "a");
-            System.out.print(FundingRaised.where(options).size());
-        } catch(IOException e) {
-            System.out.print(e.getMessage());
-            System.out.print("error");
+            val options: MutableMap<String?, String?> = HashMap<String?, String?>()
+            options.put("company_name", "Facebook")
+            options.put("round", "a")
+            print(where(options).size)
+        } catch (e: IOException) {
+            print(e.message)
+            print("error")
         }
     }
 }
